238b2f39b9709653cf20376f7132ca56ca7b1559
Tomo Yamamoto <tomoyoshi.usa@gmail.com>
2020-01-11 22:13:19 +0900

    Create synthesis.py

---
+++ synthesis.py
@@ -0,0 +1,5 @@
+# Synthesis Learning Algorithm
+# Programmed and Maintained by Tomoyoshi Yamamoto (github@tomocode)
+
+# Please note that this release is a version to demonstrate the algorithm, with no additional features. Therefore, this release is limited of its capabilities.
+# Please read README or Projects in GitHub repo site for future development.
